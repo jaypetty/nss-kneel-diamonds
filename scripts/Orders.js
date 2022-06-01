9d8a5b246903fad9338c6180db74bf1a7b82b5d9
jaypetty <jayshaun2343@gmail.com>
2022-06-01 13:24:49 -0500

    finished chapter 9

--- scripts/Orders.js
+++ scripts/Orders.js
@@ -1,11 +1,45 @@
-import { getOrders } from "./database.js"
+import { getOrders, getMetals, getStyles, getSizes } from "./database.js"
+
+const styles = getStyles()
+const sizes = getSizes()
+
+
+
 
 const buildOrderListItem = (order) => {
-    return `<li>
-        Order #${order.id} was placed on ${order.timestamp}
-    </li>`
-}
 
+    const metals = getMetals()
+
+// Remember that the function you pass to find() must return true/false
+const foundMetal = metals.find(
+    (metal) => {
+        return metal.id === order.metalId
+    }
+)
+
+const foundSize = sizes.find(
+    (size) => {
+        return size.id === order.sizeId
+    }
+)
+
+const foundStyle = styles.find(
+    (style) => {
+        return style.id === order.styleId
+    }
+)
+const totalCost = foundMetal.price + foundSize.price + foundStyle.price
+
+const costString = totalCost.toLocaleString("en-US", {
+    style: "currency",
+    currency: "USD"
+})
+
+return `<li>
+    Order #${order.id} cost ${costString}
+</li>`
+}
+   
 export const Orders = () => {
     /*
         Can you explain why the state variable has to be inside
@@ -15,7 +49,7 @@ export const Orders = () => {
 
     let html = "<ul>"
 
-    const listItems = orders.map(buildOrderListItem)
+    const listItems = orders.map(orderList => {return buildOrderListItem(orderList)})
 
     html += listItems.join("")
     html += "</ul>"
